335d927cc46e0ceb928a75d3c1000461ec846ff2
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-02-28 15:45:52 -0500

    Update position.cpp

--- position.cpp
+++ position.cpp
@@ -1,1 +1,44 @@
-
+//Boundless Position
+class Position
+{
+  private:
+  int x;
+  int y;
+  public:
+  Position(int x,int y);
+  Position(void);
+  int GetX(void)
+  {
+    return this->x;
+  }
+  int GetY(void)
+  {
+    return this->y;
+  }
+  void left(void)
+  {
+    (this->x)--;
+  }
+  void right(void)
+  {
+    (this->x)++;
+  }
+  void up(void)
+  {
+    (this->y)++;
+  }
+  void down(void)
+  {
+    (this->y)--;
+  }
+};
+Position::Position(int x,int y)
+{
+  this->x=x;
+  this->y=y;
+}
+Position::Position(void)
+{
+  this->x=0;
+  this->y=0;
+}
