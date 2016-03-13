cb9c633855af43ec0c9645fe1a7eda5cae0a0f41
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-12 20:58:25 -0500

    Update fs.py

--- fs.py
+++ fs.py
@@ -1,1 +1,25 @@
 class FSFile:
+  def __init__(self):
+    self.text=""
+  def ReadText(self):
+    return self.txt
+  def WriteText(self,new):
+    self.text=new
+
+class FSFolder:
+  def __init__(self):
+    self.contents={'.':self}
+  def ReadFile(self,item):
+    return self.contents[item].ReadText()
+  def WriteFile(self,item,val):
+    return self.contents[item].WriteText(val)
+  def TouchFile(self,item):
+    return self.contents[item]=FSFile()
+  def DelFile(self,item):
+    del self.contents[item]
+  def GetFolder(self,item):
+    return self.contents[item]
+  def TouchFolder(self,item):
+    self.contents[item]=FSFolder()
+  def DelFolder(self,item):
+    del self.contents[item]
