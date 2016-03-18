5883d099c5c5ff21b223b26d38fb4b3bdb2b4e43
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-03-18 13:32:45 -0400

    Update stdio.py

--- stdio.py
+++ stdio.py
@@ -1,2 +1,13 @@
-from __future__ import 
-class Stream:
+class OStream:
+  def __init__(self):
+    pass
+  def __str__(self):
+    return None
+  def __lshift__(self,arg):
+    import sys
+    sys.stdout.write(str(arg))
+    return OStream()
+import os
+endl='\n'
+cout = OStream()
+cout << "Hello World" << endl << "New " << "line"
