1b085d45e18c6ea02a12513b0fe3a1cfdb85701a
zacharyacutey <zacharyacutey@users.noreply.github.com>
2016-04-04 16:10:15 -0400

    Update bf_c_transpiler.py

--- bf_c_transpiler.py
+++ bf_c_transpiler.py
@@ -1,1 +1,26 @@
 def instruction(arg):
+  if arg=="+":
+    return "++p;"
+  if arg=="-":
+    return "--p;"
+  if arg=="<":
+    return "--*p;"
+  if arg==">":
+    return "++*p;"
+  if arg=="[":
+    return "while(*p){"
+  if arg=="]":
+    return "}"
+  if arg==".":
+    return "putchar(*p);"
+  if arg==",":
+    return "*p=getchar();"
+  return ""
+INIT="#include <stdio.h>\nint main(){"
+END="}"
+def transpiler(arg):
+  return INIT+"".join([instruction(i) for i in arg])+END
+def transpile(name):
+  f=open(name)
+  g=open(name+".c","w")
+  g.write(transpiler(f.read()))
